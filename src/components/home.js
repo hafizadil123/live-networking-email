f6c32f9310cfcb2e5ccfa8c7ef2238f4495f6672
Adil Sikandar <adil.sikandar@mobilelive.ca>
2021-03-13 19:26:21 +0500

    fix changes

--- src/components/home.js
+++ src/components/home.js
@@ -12,7 +12,7 @@ function Home(){
    <div>
   <div className="container-fluid text-center" style={{marginTop: '100px'}}>
     <h1 className="font-weight-bold display-4">Aggnett.com</h1>
-    <img src={logo} className="rounded-circle" alt="logo" width={100} height={100} />
+    <img src="https://i.ibb.co/jTFKRSN/logo.jpg" className="rounded-circle" alt="logo" width={100} height={100} />
     <p className="font-weight-bold text-secondary">Select club</p>
     <div className="club font-weight-bold display-5 w-25 mx-auto">
       {clubNames && clubNames.length > 0 && clubNames.map(item => <><Link to={`/store?club=${item.value}`} style={{ textDecoration: 'none' }}>{item.name}</Link><br /></>)}
--- src/components/store.js
+++ src/components/store.js
@@ -27,7 +27,7 @@ function Store(){
   <input type="search" className="form-control rounded" placeholder={`${placeholderObj.name} - Filter Category..`} aria-label="Search"  value={searchText} aria-describedby="search-addon" onChange={e => search(e.target.value)}/>
 </div>
   </div>
-  <div className="store" style={{marginTop: '8%'}}>
+  <div className="store" style={{marginTop: '4%'}}>
     <h5>2- Select Store Type:</h5>
   </div>
   <div className="store-box mx-auto">
